429a9a009b9b038e47b433c2f03fe9e729c192dc
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-27 14:05:46 +0700

    set the route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,53 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter } from 'react-router-dom'
+
+import ProductCategory from './pages/category/ProductCategory'
+import Product from './pages/product/Product.jsx'
+import Cart from './pages/cart/Cart.jsx'
+import Home
+ from './pages/home/Home.jsx'
+import { RouterProvider } from 'react-router'
+const router = createBrowserRouter([
+  {
+    path:'/',   //Template
+    element:<App/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:'/categories/electronics',
+        element:<ProductCategory/>
+      },
+      {
+        path:'/categories/jewelery',
+        element:<ProductCategory/>
+      },
+      {
+        path:"/categories/men's clothing",
+        element:<ProductCategory/>
+      },
+      {
+        path:"/categories/women's clothing",
+        element:<ProductCategory/>
+      },
+      {
+        path:"/products/:id",
+        element:<Product/>
+      },
+      {
+        path:"/cart",
+        element:<Cart/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <RouterProvider router={router}>
     <App />
-  </StrictMode>,
+  </RouterProvider>,
 )
