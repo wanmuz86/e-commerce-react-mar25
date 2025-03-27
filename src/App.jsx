fc8004ad9feb561d679f2193409a1c7decbf0eeb
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-27 14:43:37 +0700

    add navigation

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { Outlet } from 'react-router-dom'
 
 function App() {
  
-
   return (
     <>
      <Header/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <header className='p-5 bg-danger text-warning text-center mb-3'>
-        <h1>Lazamall</h1>
-    </header>
-  )
+    return (
+        <>
+            <header className='p-5 bg-danger text-warning text-center'>
+                <h1>Lazamall</h1>
+            </header>
+            {/* navbar-light : Navbar is light colour you want the text to be dark
+              , navbar-dark : Navbar is dark colour, you want the text to be light */}
+            <nav className="navbar navbar-expand-lg bg-success  mb-3 navbar-dark">
+                <div className="container">
+                    <NavLink className="navbar-brand" to="/">Lazamall</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/categories/men's clothing">Men's Clothing</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/categories/women's clothing">Women's Clothing</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/categories/jewelery">Jewelery</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/categories/electronics">Electronics</NavLink>
+                            </li>  
+                        </ul>
+                    </div>
+                    <div className="d-flex">
+                        <button className="btn btn-outline-light"><i class="bi bi-cart"></i></button>
+                    </div>
+                </div>
+                
+            </nav>
+        </>
+    )
 }
 
 export default Header
