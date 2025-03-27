78fd1703a6180fbda2928ab1d9f9d22048e7b8b1
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-27 14:16:36 +0700

    add routing outlet , header and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='p-5 bg-danger text-warning text-center mb-3'>
+        <h1>Lazamall</h1>
+    </header>
+  )
+}
+
+export default Header
