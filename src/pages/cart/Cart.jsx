4ba364c73b1548d0af941a0f0dc7216ab82258f6
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-28 11:08:22 +0700

    Show the item in cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { use } from 'react'
 import './Cart.css'
+import { useCart } from '../../context/CartContext'
 
 const Cart = () => {
+  const {state, dispatch} = useCart()
+
   return (
-    <div>Cart</div>
+    <div className='container'>
+      <h2>Cart</h2>
+      {
+        state.cart?.length === 0 ?
+        <p>No item in the cart yet. Start shopping now!</p>
+        :
+        <div>
+          {
+            state.cart?.map(val=><p>{val.item.title} - {val.quantity}</p>)
+          }
+        </div>
+      }
+    </div>
   )
 }
 
